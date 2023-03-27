2f67ed0cec9f256692bac62cc7507e3c865a99e7
Panfilov16 <panfilov.a.e@mail.ru>
2023-03-27 19:31:52 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        bfdngfnf
         <h2>Счетчик:</h2>
         <h1>0</h1>
         <button className="minus">- Минус</button>
